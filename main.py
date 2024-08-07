46c52195628a166680dc44c829f49f796caf1189
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-08-08 01:50:19 +0300

    Use system webview if available (#2)

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import asyncio
-from modules import server
+import threading
+from modules import server, webview
+from env import CFG
+
+
+def main():
+    # port = CFG['uvicorn'].get('port', 15010)
+    port = CFG['uvicorn'].get('port', server.find_free_port())
+    host = CFG['uvicorn'].get('host', '127.0.0.1')
+
+    async_thread = threading.Thread(target=server.run, args=(host, port))
+    async_thread.start()
+    webview.open(f"{host}:{port}")
+    async_thread.join()
+
 
 if __name__ == '__main__':
-    asyncio.run(server.run())
+    main()
